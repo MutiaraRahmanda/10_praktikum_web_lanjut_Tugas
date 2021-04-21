5e0e12f1b84a12d2b67478d811174ada8bc4e5eb
= <=>
2021-04-22 00:08:30 +0700

    latihan praktikum 9

---
+++ database/seeders/MahasiswaMatakuliahSeeder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MahasiswaMatakuliahSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $matkul = [
+            [
+                'mahasiswa_id' => '1931720189',
+                'matakuliah_id' => '1',
+                'nilai' => 'A',
+            ],
+            [
+
+                'mahasiswa_id' => '1931720189',
+                'matakuliah_id' => '2',
+                'nilai' => 'A',
+            ],
+            [
+
+                'mahasiswa_id' => '1931720189',
+                'matakuliah_id' => '3',
+                'nilai' => 'A',
+            ],
+            [
+                'mahasiswa_id' => '1931720189',
+                'matakuliah_id' => '4',
+                'nilai' => 'A',
+            ],
+
+            [
+                'mahasiswa_id' => '1941720034',
+                'matakuliah_id' => '1',
+                'nilai' => 'B',
+            ],
+            [
+                'mahasiswa_id' => '1941720034',
+                'matakuliah_id' => '2',
+                'nilai' => 'B',
+            ],
+            [
+                'mahasiswa_id' => '1941720034',
+                'matakuliah_id' => '3',
+                'nilai' => 'A',
+            ],
+            [
+                'mahasiswa_id' => '1941720034',
+                'matakuliah_id' => '4',
+                'nilai' => 'B',
+            ],
+        ];
+
+        DB::table('mahasiswa_matakuliah')->insert($matkul);
+    }
+}
---
+++ database/seeders/UpdateMahasiswaSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Database\Seeder;
+
+class UpdateMahasiswaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //misal kita update data mahasiswa yang ada saat ini milik TI 2A
+        DB::table('mahasiswa')->update(['kelas_id' => 1]);
+    }
+}
